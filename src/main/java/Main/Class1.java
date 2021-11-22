88a26613a95bb59aabe3a4a31aa836e12c248558
Jillul Islam <jillul.islam@outlook.com>
2021-11-22 14:07:35 +0000

    Removed main method

--- src/main/java/Main/Class1.java
+++ src/main/java/Main/Class1.java
@@ -2,14 +2,7 @@ package Main;
 
 public class Class1 {
 
-    public static void main(String[] args) {
-        Class1 class1 = new Class1();
-        System.out.println((class1.addition(2, 5)));
-
-    }
-
     public double addition(double input1, double input2){
-
         double sum = input1 + input2;
         return sum;
     }
